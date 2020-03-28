3a6b3679b36eda143476756fe3a6b4a65ed800eb
NuttyPanu <misterpanukorn@gmail.com>
2020-03-28 15:38:03 +0700

    Update liff-starter.js

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -1,7 +1,10 @@
 window.onload = function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "1653890083-d5XZqBjy";   // change the default LIFF value if you are not using a node server
-
+    //const defaultLiffId = "1653890083-d5XZqBjy";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1653970390-EDvl28PQ";  
+    
+    
+    
     // DO NOT CHANGE THIS
     let myLiffId = "";
 
